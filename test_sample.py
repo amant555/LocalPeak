803863c4a52716f15a6f7cede11bc0b75970462d
Aman Tiwari <amant5186@gmail.com>
2020-02-18 17:12:06 +0530

    Add: PeakFinder If input contain three element that is bigger than surrounding will be the peak

--- test_sample.py
+++ test_sample.py
@@ -5,10 +5,13 @@ def peak(array):
     if len(array) == 0:
         return None
     if len(array) > 1:
-        if array[0] > array[1]:
-            return array[0]
-        else:
-            return array[1]
+        for i in range(len(array)):
+            if i == 0 and array[i] > array[i + 1]:
+                return array[i]
+            elif i == len(array) - 1 and array[i] > array[i - 1]:
+                return array[i]
+            elif array[i]>array[i+1] and array[i]>array[i-1]:
+                return array[i]
     return array[0]
 
 
@@ -21,6 +24,9 @@ class PeakFinder(unittest.TestCase):
 
     def test_input_with_two_element_then_the_bigger_element_will_be_the_peak(self):
         self.assertEqual(2, peak([1, 2]))
+
+    def test_input_with_three_element_then_the_element_that_is_bigger_than_surrounding_will_be_the_peak(self):
+        self.assertEqual(2, peak([1, 2, 1]))
 
 
 if __name__ == '__main__':
