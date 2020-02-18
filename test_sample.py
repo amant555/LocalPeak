50b6bf651e2c95d8fb834daaaf8d12157cfafe28
Aman Tiwari <amant5186@gmail.com>
2020-02-18 16:42:18 +0530

    Add: PeakFinder If input contain one element then that is the peak

--- test_sample.py
+++ test_sample.py
@@ -11,6 +11,9 @@ class PeakFinder(unittest.TestCase):
     def test_input_with_no_element_then_there_is_no_peak(self):
         self.assertEqual(None, peak([]))
 
+    def test_input_with_one_element_then_that_element_will_be_the_peak(self):
+        self.assertEqual(1, peak([1]))
+
 
 if __name__ == '__main__':
     unittest.main()
