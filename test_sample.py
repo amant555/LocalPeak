550611fbfb9ad94f8ed762e52a7fc0d58561c352
Aman Tiwari <amant5186@gmail.com>
2020-02-18 16:52:13 +0530

    Add: PeakFinder If input contain two element then largest is the peak

--- test_sample.py
+++ test_sample.py
@@ -2,8 +2,13 @@ import unittest
 
 
 def peak(array):
-    if (len(array) == 0):
+    if len(array) == 0:
         return None
+    if len(array) > 1:
+        if array[0] > array[1]:
+            return array[0]
+        else:
+            return array[1]
     return array[0]
 
 
@@ -13,6 +18,9 @@ class PeakFinder(unittest.TestCase):
 
     def test_input_with_one_element_then_that_element_will_be_the_peak(self):
         self.assertEqual(1, peak([1]))
+
+    def test_input_with_two_element_then_the_bigger_element_will_be_the_peak(self):
+        self.assertEqual(2, peak([1, 2]))
 
 
 if __name__ == '__main__':
